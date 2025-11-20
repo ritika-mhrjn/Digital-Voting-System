dd062178aff13d189159f2b81d265ce40ea469a3
Kriii1ii <achkri157@gmail.com>
2025-11-20 15:23:10 +0545

    jwt

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
-import { jwtDecode } from "jwt-decode";
+import  jwtDecode  from "jwt-decode";
 
 const AuthContext = createContext(null);
 
